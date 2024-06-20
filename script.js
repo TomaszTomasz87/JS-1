8cbbf3fe6c6f97ea76e15ee2b4e6abbd2a481d00
TomaszTomasz87 <146130840+TomaszTomasz87@users.noreply.github.com>
2024-06-20 14:17:06 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,17 @@
+const redBtn = document.querySelector('#btn1')
+const blueBtn = document.querySelector('.btn2')
+const square = document.querySelector('.color')
+const removeColorBtn = document.querySelector('.remove-color')
+
+function redColor() {
+    square.classList.add('red')
+    square.classList.remove('blue')
+}
+
+function blueColor() {
+    square.classList.add('blue')
+    square.classList.remove('red')
+}
+redBtn.addEventListener('click', redColor)
+
+blueBtn.addEventListener('click', blueColor)
